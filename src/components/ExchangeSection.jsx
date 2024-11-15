fca1de4f3a13ff114ec6f553796714e8f6d9a629
Melina <yunoantognetti@gmail.com>
2024-11-14 21:21:12 -0300

    aaaaaaaaaa

---
+++ src/components/ExchangeSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ExchangeSection = () => {
+  return (
+    <section className="main-exchange-container">
+      
+      <div className="main-exchange-container--title">
+        <h2>Traemos información en tiempo real de las casas de cambio</h2>
+        <h1>y las monedas más importantes del mundo.</h1>
+      </div>
+    </section>
+  );
+};
+
+export default ExchangeSection;
